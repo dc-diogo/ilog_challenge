e88039221d4014c8a60b3cb223f6017e8e10dd4e
Diogo Viana <diogocampos@outlook.com>
2020-12-09 20:46:54 -0300

    Creating "adding course" method and separating responsibilities from service and controller.

---
+++ src/main/java/com/challenge/ilog/course/CourseController.java
@@ -0,0 +1,40 @@
+package com.challenge.ilog.course;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.Optional;
+
+@Controller
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    public Iterable<Course> getAllCourses(){
+        return courseRepository.findAll();
+    }
+
+    public Course getCourseById(int identifier){
+        Optional<Course> courseById = courseRepository.findById(identifier);
+        return courseById.orElse(null);
+    }
+
+    public String addCourse(Course course){
+        createNewCourse(course);
+        return null;
+    }
+
+    private Course createNewCourse(Course course){
+        Course courseToAdd = new Course(
+                course.getCourseName(),
+                course.getCourseDescription(),
+                course.getDuration(),
+                course.getCoursePrice()
+        );
+        return courseRepository.save(courseToAdd);
+    }
+}
--- src/main/java/com/challenge/ilog/course/CourseService.java
+++ src/main/java/com/challenge/ilog/course/CourseService.java
@@ -2,28 +2,30 @@ package com.challenge.ilog.course;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 
 @Controller
+@RequestMapping(path="/course")
 public class CourseService {
 
     @Autowired
-    private CourseRepository courseRepository;
+    private CourseController courseController;
 
-    @GetMapping(path="all")
+    @GetMapping(path="/all")
     public @ResponseBody Iterable<Course> getAllCourses() {
-        return courseRepository.findAll();
+        System.out.println("teste");
+        return courseController.getAllCourses();
     }
 
-    @GetMapping(path="course")
+    @GetMapping(path="/findById")
     public @ResponseBody Course getAllCourses(@RequestParam() int identifier){
-        Optional<Course> courseById = courseRepository.findById(identifier);
-        return courseById.orElse(null);
+        return courseController.getCourseById(identifier);
+    }
+
+    @PostMapping(path="/add")
+    public @ResponseBody String addNewCourse (@RequestBody Course course) {
+        return courseController.addCourse(course);
     }
 
 
